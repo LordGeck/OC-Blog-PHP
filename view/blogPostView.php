ae151b6999b5a5428ce6d28cf086f4f70cc135e0
LordGeck <lordgeck@gmail.com>
2021-02-01 17:40:10 +0100

    adding blogPostView.php

---
+++ view/blogPostView.php
@@ -0,0 +1,37 @@
+<?php $title = htmlspecialchars($blogPost['title']); ?>
+
+<?php ob_start(); ?>
+
+<header class="masthead" style="">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="post-heading">
+                    <h1>
+                        <?= htmlspecialchars($blogPost['title']) ?>
+                    </h1>
+                    <h2 class="subheading">
+                        <?= htmlspecialchars($blogPost['header_content']) ?>
+                    </h2>
+                    <span class="meta">Publié par <?= htmlspecialchars($blogPost['first_name']) ?> <?= htmlspecialchars($blogPost['last_name']) ?>. le <?= htmlspecialchars($blogPost['update_date']) ?></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<article>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <p>
+                    <?= htmlspecialchars($blogPost['main_content']) ?>
+                </p>
+            </div>
+        </div>
+    </div>
+</article>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
