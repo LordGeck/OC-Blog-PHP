82b03f61125765ae83cbb0c55f8fbb0eaf2c747b
LordGeck <lordgeck@gmail.com>
2021-02-08 12:02:47 +0100

    UserManager update and new addUser method

--- model/UserManager.php
+++ model/UserManager.php
@@ -4,11 +4,18 @@ require_once("model/Manager.php");
 
 class UserManager extends Manager
 {
-    public function getUser(int $username)
+    public function getUser(string $username)
     {
         $request = $this->database->prepare('SELECT username, email, first_name, last_name, password_hash, role FROM users WHERE username = ?');
         $request->execute([$username]);
 
-        return $request;
+        return $request->fetch();
+    }
+
+    public function addUser(string $username, $email, $first_name, $last_name, $password_hash)
+    {
+        $request = $this->database->prepare('INSERT INTO users(username, email, first_name, last_name, password_hash) VALUES(?, ?, ?, ?, ?)');
+
+        return $request->execute([$username, $email, $first_name, $last_name, $password_hash]);
     }
 }
