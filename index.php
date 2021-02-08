e73647fb8b959dba0059400a3d85dd3f4420103f
LordGeck <lordgeck@gmail.com>
2021-02-08 12:06:56 +0100

    router update for signup

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 require('controller/homeController.php');
 require('controller/blogPostController.php');
+require('controller/signupController.php');
 
 try {
     if (isset($_GET['page'])) {
@@ -16,9 +17,27 @@ try {
                 throw new Exception('Id d\'article absent.');                
             }
         }
+        elseif ($_GET['page'] === 'signup') {
+            signupPage();
+        }
         else {
             throw new Exception('Cette page n\'existe pas.');
-        }        
+        }       
+    }
+    elseif (isset($_GET['action'])) {
+        if ($_GET['action'] === 'signup') {
+            if (!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['password_1']) && !empty($_POST['password_2'])) {
+                if ($_POST['password_1'] != $_POST['password_2']) {
+                    throw new Exception('Les mots de passe ne correspondent pas.');
+                }
+                else {
+                    signup($_POST['username'], $_POST['email'], $_POST['first_name'], $_POST['last_name'], $_POST['password_1']);
+                }                
+            }
+            else {
+                throw new Exception('Certains champs sont vides.');
+            }
+        }
     }
     else {
         home();
