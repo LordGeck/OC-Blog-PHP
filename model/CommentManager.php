ac6b73222aa8bd85f4c7e93dbb21e69ac72be4d8
LordGeck <lordgeck@gmail.com>
2021-02-27 23:07:38 +0100

    new manager and controller to display comments in blog pages

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -4,10 +4,10 @@ require_once('model/Manager.php');
 
 class CommentManager extends Manager
 {
-    public function getPostComments(int $blogPostId): array
+    public function getPostComments(int $blogPostId, string $status): object
     {
-        $request = $this->database->prepare('SELECT id, status, creation_date, update_date, content, blog_post_id, user_username FROM post_comments WHERE blog_post_id = ?');
-        $request->execute([$blogPostId]);
+        $request = $this->database->prepare('SELECT id, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date, DATE_FORMAT(update_date, \'%d/%m/%Y à %Hh%imin%ss\') AS update_date, content, firstname, SUBSTRING(lastname, 1, 1) AS lastname FROM post_comments INNER JOIN users ON username = user_username WHERE blog_post_id = ? AND status = ? ORDER BY creation_date DESC');
+        $request->execute([$blogPostId, $status]);
 
         return $request;
     }
