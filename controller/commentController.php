2fe1d64b47140ecac4cea99b49a6f61de3606a9b
LordGeck <lordgeck@gmail.com>
2021-03-08 08:39:32 +0100

    new controller for comments

---
+++ controller/commentController.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+require_once 'model/BlogPostManager.php';
+require_once 'model/CommentManager.php';
+
+function addComment(string $content, string $blogPostId, string $username): void
+{
+    $commentManager = new CommentManager();
+    $commentManager->addPostComment($content, (int)$blogPostId, $username);
+    blogPost('Commentaire ajouté et en attente de validation.', 'success');
+}
