2de7b0771f0c1f03e4e7d1a5fa17d68dcc05d89b
LordGeck <lordgeck@gmail.com>
2021-03-15 08:52:07 +0100

    post edition view and new edition button on post page

--- view/blogPostView.php
+++ view/blogPostView.php
@@ -32,6 +32,19 @@
         </div>
     </div>
 </article>
+<?php 
+if (isset($_SESSION['username']) && $_SESSION['role'] === 'ADMIN') {
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <a class="btn btn-primary" href="index.php?page=editPost&amp;id=<?= $blogPost['id'] ?>" role="button">Modifier</a>
+        </div>
+    </div>
+</div>
+<?php
+}
+?>
 <hr>
 <div class="container">
     <div class="row">
