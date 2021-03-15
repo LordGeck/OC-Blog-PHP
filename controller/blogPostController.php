9e36e98300f96d3ee31aa08faa0a05ea10fe2ca2
LordGeck <lordgeck@gmail.com>
2021-03-15 08:50:32 +0100

    controller update

--- controller/blogPostController.php
+++ controller/blogPostController.php
@@ -36,3 +36,17 @@ function addBlogPost(string $title, string $headerContent, string $mainContent, 
     $blogPostManager->addBlogPost($title, $headerContent, $mainContent, $username);
     header('Location: index.php?page=postList');
 }
+
+function editBlogPostPage(string $message = null, string $type = null): void
+{
+    $blogPostManager = new BlogPostManager();
+    $blogPost = $blogPostManager->getBlogPost((int)$_GET['id']);
+    require 'view/editBlogPostView.php' ;
+}
+
+function editBlogPost(string $blogPostId, string $title, string $headerContent, string $mainContent): void
+{
+    $blogPostManager = new BlogPostManager();
+    $blogPostManager->setBlogPost((int)$blogPostId, $title, $headerContent, $mainContent);
+    blogPost('Article modifié avec succès', 'success');
+}
