1f4e941031a540c4ba4d497ffb6f1804dc05109d
Garald <lordgeck@gmail.com>
2021-03-22 10:36:34 +0100

    error page new style (#63)

--- view/errorView.php
+++ view/errorView.php
@@ -8,12 +8,19 @@
         <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
                 <div class="site-heading">
-                    <h1>Erreur : <?= htmlspecialchars($error) ?></h1>
+                    <h1>Le site a rencotré un problème.</h1>
                 </div>
             </div>
         </div>
     </div>
 </header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="alert alert-danger" role="alert">Erreur : <?= htmlspecialchars($error) ?></div>
+        </div>
+    </div>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 
