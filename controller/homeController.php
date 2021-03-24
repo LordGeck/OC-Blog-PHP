0aee9041a40503438a22bb5a191fc9994ac6164c
Garald <lordgeck@gmail.com>
2021-03-24 17:19:54 +0100

    home page contact form (#65)
    * home page contact form
    
    * phpmailer implementation with composer

--- controller/homeController.php
+++ controller/homeController.php
@@ -1,7 +1,36 @@
 <?php
 declare(strict_types=1);
+use PHPMailer\PHPMailer\PHPMailer;
 
-function home(): void
+require 'vendor/autoload.php';
+
+function home(string $message = null, string $type = null): void
 {
     require 'view/homeView.php';
+}
+
+function sendMail(string $name, string $email, string $message): void
+{
+    $mail = new PHPMailer;
+    $mail->setFrom('admin@blogphp.com');
+    $mail->addAddress('adress@gmail.com');
+    $mail->addReplyTo($_POST['email'], $_POST['name']);
+    $mail->Subject = 'Formulaire de contact : message de ' . $_POST['name'];
+    $mail->Body = $_POST['message'];
+    $mail->IsSMTP();
+    $mail->SMTPSecure = 'ssl';
+    $mail->Host = 'ssl://smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Port = 465;
+
+    //Set your existing gmail address as user name
+    $mail->Username = 'adress@gmail.com';
+
+    //Set the password of your gmail address here
+    $mail->Password = 'PASSWORD';
+    if (!$mail->send()) {
+        home('Erreur d\'envoi.', 'danger');
+    } else {
+        home('E-mail envoyé.', 'success');
+    }
 }
