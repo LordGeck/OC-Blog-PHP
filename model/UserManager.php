b1105970c8da7e907e77252aa955ceaecb8d5a37
LordGeck <lordgeck@gmail.com>
2021-03-24 18:08:35 +0100

    username and email existence verification method

--- model/UserManager.php
+++ model/UserManager.php
@@ -14,6 +14,16 @@ class UserManager extends Manager
         return $request->fetch();
     }
 
+    public function checkUser(string $username, string $email): array|false
+    {
+        $request = $this->database->prepare('SELECT username, email
+            FROM users
+            WHERE username = ? OR email = ?');
+        $request->execute([$username, $email]);
+
+        return $request->fetch();
+    }
+
     public function addUser(string $username, string $email, string $firstname, string $lastname, string $passwordHash): bool
     {
         $request = $this->database->prepare('INSERT INTO users
